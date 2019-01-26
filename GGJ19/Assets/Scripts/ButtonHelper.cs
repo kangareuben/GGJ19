4b7b34d99408bef05f8301775ae365b5e02f5261
Reuben Brenner-Adams <rmb1952@rit.edu>
2019-01-26 11:10:14 -0500

    Added game speed slider, added TextMesh Pro assets

---
+++ GGJ19/Assets/Scripts/ButtonHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHelper : MonoBehaviour
+{
+    [SerializeField]
+    private Slider gameSpeedSlider;
+    [SerializeField]
+    private TextMeshProUGUI gameSpeedLabel;
+
+    public void SetGameSpeed()
+    {
+        GameManager.instance.SetGameSpeed(gameSpeedSlider.value);
+        gameSpeedLabel.text = "Game Speed: " + gameSpeedSlider.value.ToString("F1");
+    }
+}
---
+++ GGJ19/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private float gameSpeed;
+
+    public float GameSpeed
+    {
+        set
+        {
+            gameSpeed = value;
+            Time.timeScale = gameSpeed;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetGameSpeed(float speed)
+    {
+        GameSpeed = speed;
+    }
+}
