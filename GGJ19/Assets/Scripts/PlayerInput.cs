f9be254a03a65abd1c3019bc4359434fda409836
Matt Zeosky <theskipper360@gmail.com>
2019-01-25 21:34:16 -0500

    Creating a launcher to eventually connect stars together

---
+++ GGJ19/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _traceGO;
+    [SerializeField]
+    private LaunchSettings _launchSettings;
+
+    private Transform _currentTransform;
+
+    private float _launchPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _launchPower = _launchSettings.baseLaunchPower;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            FireTrace();
+        }
+    }
+
+    private void FireTrace()
+    {
+        Vector3 transformScreenPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 direction = Vector3.Normalize(Input.mousePosition - transformScreenPos);
+
+        Tracer trace = Instantiate(_traceGO).GetComponent<Tracer>();
+        trace.Launch(direction * _launchPower);
+    }
+
+    [System.Serializable]
+    private struct LaunchSettings
+    {
+        public float baseLaunchPower;
+        public float maxLaunchPower;
+        public float launchScaleRate;
+    }
+}
---
+++ GGJ19/Assets/Scripts/Tracer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracer : MonoBehaviour
+{
+    [SerializeField]
+    private float _lifetime = 2f;
+
+    private Vector3 _velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {
+        transform.position += _velocity;
+    }
+
+    public void Launch(Vector3 velocity)
+    {
+        _velocity = velocity;
+        Destroy(gameObject, _lifetime);
+    }
+}
